7abfbf16bb1152587485f09c55056f343c595044
ebalo <ebalo55@users.noreply.github.com>
2023-02-21 17:38:08 +0100

    assets publishing

---
+++ resources/views/vendor/notifications/components/dropdown/item.blade.php
@@ -0,0 +1,8 @@
+@captureSlots([
+'detail',
+])
+
+<x-notifications::dropdown.list.item
+    :attributes="\Filament\Support\prepare_inherited_attributes($attributes)->merge($slots)">
+    {{ $slot }}
+</x-notifications::dropdown.list.item>
---
+++ resources/views/vendor/scribe/components/badges/auth.blade.php
@@ -0,0 +1,4 @@
+@if($authenticated)
+    @component('scribe::components.badges.base', ['colour' => "darkred", 'text' => 'requires authentication'])
+    @endcomponent
+@endif
